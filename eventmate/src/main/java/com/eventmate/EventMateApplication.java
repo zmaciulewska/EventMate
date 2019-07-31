dc53983291c497d742e3339052260aa71df1796d
Zuzanna Maciulewska <zmaciulewska@gmail.com>
2019-07-31 19:17:08 +0200

    Initial commit

---
+++ eventmate/src/main/java/com/eventmate/EventMateApplication.java
@@ -0,0 +1,13 @@
+package com.eventmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventMateApplication.class, args);
+    }
+
+}
